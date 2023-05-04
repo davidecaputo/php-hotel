37382491ab3a299ddba2ad6da710e0c528c17440
davidecaputo <daca9621@gmail.com>
2023-05-04 18:05:04 +0200

    Add elements

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
         ],
 
     ];
-    
+
 ?>
 
 <!DOCTYPE html>
@@ -52,6 +52,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-
+    <div class="d-flex">
+    <?php for ($i = 0; $i < count($hotels); $i++) { ?>
+        <ul>
+            <li><?php echo $hotels[$i]['name'] ?></li>
+            <li><?php echo $hotels[$i]['description'] ?></li>
+            <li><?php echo $hotels[$i]['parking'] ?></li>
+            <li><?php echo $hotels[$i]['vote'] ?></li>
+            <li><?php echo $hotels[$i]['distance_to_center'] ?></li>
+        </ul>
+    <?php } ?>
+    <?php foreach ($hotels as $key => $value) {
+        echo $hotels;
+    } ?>
+    </div>
 </body>
 </html>
